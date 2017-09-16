8460d440f7c6d6882437c57235da6b4baac4ef7d
mohankrishna12 <31994001+mohankrishna12@users.noreply.github.com>
2017-09-16 13:18:02 +0530

    project1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		 Scanner g=new Scanner(System.in);
+		 StudentGroup sg=new StudentGroup(g.nextInt());
+                                 sg.getStudents();
+		 sg.setStudents();
+		 sg.getStudent(3);
+		 
+	}
+
+}
